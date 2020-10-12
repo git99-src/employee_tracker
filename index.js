eaba1d1c28b0049a2934a83281e0a00c532a2a8a
git99.src <cpu.al2@gmail.com>
2020-10-12 10:03:42 -0700

    Added menuing type system logic and testing for add employee

--- index.js
+++ index.js
@@ -48,6 +48,9 @@ function main() {
         case "View All Employees By Manager":
           viewAllEmployeesManager();
           break;
+        case "Add Employee":
+          addEmployee();
+          break;    
         case "Exit":
           connection.end();
           break;
@@ -90,8 +93,8 @@ function viewAllEmployees() {
 };
 
 function viewAllEmployeesDept() {
-  // perform query for all employees and print the result in the console
-  return connection.query("SELECT employee.id, employee.first_name, employee.last_name, department.dept_name AS department FROM employee JOIN roles ON employee.roles_id = roles.id JOIN department ON roles.department_id = department.id ORDER BY department_id", (err, res) => {
+  // perform query for all employees order by department and print the result in the console
+  return connection.query("SELECT department.dept_name AS department, employee.id, employee.first_name, employee.last_name FROM employee JOIN roles ON employee.roles_id = roles.id JOIN department ON roles.department_id = department.id ORDER BY department_id", (err, res) => {
     if (err) {
       throw err;
     };
@@ -101,12 +104,38 @@ function viewAllEmployeesDept() {
 };
 
 function viewAllEmployeesManager() {
-  // perform query for all employees and print the result in the console
+  // perform query for all employees order by manager and print the result in the console
   return connection.query("SELECT CONCAT(e.first_name, ' ' ,e.last_name) AS manager, employee.id, employee.first_name, employee.last_name, roles.title, roles.salary, department.dept_name as department FROM employee INNER JOIN roles ON roles.id = employee.roles_id INNER JOIN department ON department.id = roles.department_id LEFT JOIN employee e ON e.id = employee.manager_id ORDER BY manager", (err, res) => {
     if (err) {
       throw err;
     };
     console.table(res);
+    return main();
+  });
+};
+
+function addEmployee() {
+  //prompt for first name, last name, (query) show list of roles to select, (query) show list of managers to select, 
+  // perform query insert to add employee to employee table and print the result in the console
+  return connection.query("INSERT INTO employee SET ?", 
+  // {
+  //   first_name: answer.first_name,
+  //   last_name: answer.last_name,
+  //   roles_id: answer.roles_id,
+  //   manager_id: answer.manager_id,
+  // },
+  {
+    first_name: "Add",
+    last_name: "Dude",
+    roles_id: 6,
+    manager_id: 2,
+  },
+  (err, res) => {
+    if (err) {
+      throw err;
+    };
+    console.table(res);
+    viewAllEmployees();
     return main();
   });
 };
